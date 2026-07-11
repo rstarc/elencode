f7cee1b2b1021424f61514cdb6ab867404d4dd09
rstarc <rstarc@protonmail.com>
2026-07-11 14:59:08 +0200

    Basic REPL chat without tool use

--- cmd/tedd-code/main.go
+++ cmd/tedd-code/main.go
@@ -1,33 +1,71 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/anthropics/anthropic-sdk-go"
 	// "github.com/anthropics/anthropic-sdk-go/option"
-	// "github.com/charmbracelet/glamour"
 )
 
 func main() {
+	// TODO: Check for API Key
 	client := anthropic.NewClient()
 	// option.WithAPIKey("my-anthropic-api-key"), // defaults to os.LookupEnv("ANTHROPIC_API_KEY")
-	message, err := client.Messages.New(context.TODO(), anthropic.MessageNewParams{
-		MaxTokens: 1024,
-		Messages: []anthropic.MessageParam{
-			anthropic.NewUserMessage(anthropic.NewTextBlock("What is a quaternion?")),
-		},
-		Model: anthropic.ModelClaudeHaiku4_5,
-	})
 
-	if err != nil {
-		log.Fatal(err)
-	}
+	// TODO: Define Tools
 
-	for _, block := range message.Content {
-		if textBlock, ok := block.AsAny().(anthropic.TextBlock); ok {
-			fmt.Println(textBlock.Text)
+	var sessionMessages []anthropic.MessageParam
+	scanner := bufio.NewScanner(os.Stdin)
+
+	// REPL
+	for {
+		// Read input
+		fmt.Print("> ")
+		if !scanner.Scan() {
+			break
 		}
+		userInput := strings.TrimSpace(scanner.Text())
+		if userInput == "" {
+			continue
+		}
+		if userInput == "exit" || userInput == "quit" {
+			fmt.Println("goodbye")
+			break
+		}
+
+		fmt.Println()
+
+		// Add user message to session
+		userMessage := anthropic.NewUserMessage(anthropic.NewTextBlock(userInput))
+		sessionMessages = append(sessionMessages, userMessage)
+
+		// Evaluate response
+		response, err := client.Messages.New(context.TODO(), anthropic.MessageNewParams{
+			MaxTokens: 1024,
+			Messages:  sessionMessages,
+			Model:     anthropic.ModelClaudeHaiku4_5,
+		})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Add response to session
+		sessionMessages = append(sessionMessages, response.ToParam())
+
+		// Print output response text to user
+		for _, block := range response.Content {
+			if textBlock, ok := block.AsAny().(anthropic.TextBlock); ok {
+				fmt.Println(textBlock.Text)
+			}
+		}
+
+		fmt.Println()
+
 	}
 }
--- go.mod
+++ go.mod
@@ -2,8 +2,9 @@ module github.com/rstarc/tedd-code
 
 go 1.26.1
 
+require github.com/anthropics/anthropic-sdk-go v1.57.0
+
 require (
-	github.com/anthropics/anthropic-sdk-go v1.57.0 // indirect
 	github.com/bahlo/generic-list-go v0.2.0 // indirect
 	github.com/buger/jsonparser v1.2.0 // indirect
 	github.com/invopop/jsonschema v0.14.0 // indirect
